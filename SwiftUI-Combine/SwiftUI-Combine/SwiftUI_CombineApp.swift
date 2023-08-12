32c8aef1c8a6753f1d7ef7546a0d544ac3eea529
vinothvino42 <vinothvino42@gmail.com>
2023-08-12 14:19:32 +0530

    chore: initial commit (Template)

---
+++ SwiftUI-Combine/SwiftUI-Combine/SwiftUI_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CombineApp.swift
+//  SwiftUI-Combine
+//
+//  Created by Vinoth Vino on 12/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

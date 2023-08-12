a364c1427c93f580eaa0d0a4e73e1c2636d12335
vinothvino42 <vinothvino42@gmail.com>
2023-08-12 16:10:25 +0530

    chore: add swiftui-closure app

---
+++ SwiftUI-Closure/SwiftUI-Closure/SwiftUI_ClosureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ClosureApp.swift
+//  SwiftUI-Closure
+//
+//  Created by Vinoth Vino on 12/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ClosureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
